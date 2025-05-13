6f7295f86adf986c625866a5e5d9290ea307061e
Kanake <10kanake@gmail.com>
2025-05-14 00:56:48 +0300

    plant timber

--- app/src/main/java/com/example/weather/WeatherApp.kt
+++ app/src/main/java/com/example/weather/WeatherApp.kt
@@ -2,6 +2,16 @@ package com.example.weather
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class WeatherApp : Application()
+class WeatherApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
+
